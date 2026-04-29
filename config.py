438f8d711c127011837fa46c0d0927abdc1d165c
Govind Kashyap <165226936+Kashyapji12@users.noreply.github.com>
2026-04-30 00:56:52 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+API_ID = int(os.getenv("API_ID", "0"))
+API_HASH = os.getenv("API_HASH")
+SESSION = os.getenv("SESSION")
+OPENROUTER_KEY = os.getenv("OPENROUTER_KEY")
+
+OWNER_ID = int(os.getenv("OWNER_ID", "0"))
+SUDO_USERS = list(map(int, os.getenv("SUDO", "").split(",")))
+
+CMD = "."
