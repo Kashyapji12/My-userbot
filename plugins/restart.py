c31b31a79a75416a4a48a52f38f2a4ca1ad96831
Govind Kashyap <165226936+Kashyapji12@users.noreply.github.com>
2026-04-30 01:18:44 +0530

    Update restart.py

--- plugins/restart.py
+++ plugins/restart.py
@@ -1,3 +1,8 @@
-@client.on(events.NewMessage)
-async def log_all(e):
-    logging.info(f"{e.sender_id}: {e.text}")
+from main import client
+from telethon import events
+import os, sys
+
+@client.on(events.NewMessage(pattern=r"\.restart", outgoing=True))
+async def restart(e):
+    await e.edit("♻️ Restarting bot...")
+    os.execv(sys.executable, ["python"] + sys.argv)
